11e0b4563c8410638f2f439f75b425476ec2fc3d
balaji24k <balaji325s@gmail.com>
2023-07-30 23:02:12 +0530

    Task- 17

--- util/database.js
+++ util/database.js
@@ -1,10 +1,22 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-practice',
-    password: '246810'
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-practice',
+//     password: '246810'
+// });
+
+// module.exports = pool.promise();
+
+
+//using sequelize
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-practice', 'root', '246810', {
+    dialect : 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
